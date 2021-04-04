9a80cc50102fb453ebb5796308df4a63bc065182
odong-Tree <ss12357@naver.com>
2021-04-04 15:02:58 +0900

    feat: 배포 및 마이그레이션 정의 (#3)

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -1,10 +1,27 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+    } else {
+        // ...
+    }
 
     // register routes
     try routes(app)
 }
+
+// 심볼이 연결안되었따?
+// 컴파일러가 빌드할 때, 심볼, 링커
+
+// 심볼?
+// 
